038f053b15f17506a4dcc4f15a9dfdd8bd8b7178
Rivas1 <rivas1@cpp.edu>
2018-12-20 23:51:02 -0800

    Show view rendered
    After form is submitted, the following page renders the user's input

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -3,11 +3,23 @@ class VehiclesController < ApplicationController
 
 	end
 
+	def show
+			@vehicle = Vehicle.find(params[:id])
+	end
+
 	def new
 
 	end
 
 	def create
-		render plain: params[:vehicle].inspect
+		# render plain: params[:vehicle].inspect
+		@vehicle = Vehicle.new(vehicle_params)
+
+		@vehicle.save
+		redirect_to @vehicle
+	end
+
+	private def vehicle_params
+		params.require(:vehicle).permit(:year, :make, :model)
 	end
 end
