ebd9ec63bab68246e6565dae1b1a6e57475c35fb
Rivas1 <rivas1@cpp.edu>
2019-01-16 13:51:48 -0800

    Added reference | Removed user_id column from vehicle

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181221074051) do
+ActiveRecord::Schema.define(version: 20190116214843) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "encrypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "vehicles", force: :cascade do |t|
     t.integer "year"
